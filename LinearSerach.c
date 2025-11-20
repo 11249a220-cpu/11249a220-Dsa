dc4030824bb45282038028fc3910020bd18213c8
11249a220-cpu <11249a220@kanchiuniv.ac.in>
2025-11-20 19:41:27 +0530

    Create LinearSerach.c

---
+++ LinearSerach.c
@@ -0,0 +1,47 @@
+Aim:To find the element in an array using linear search
+
+Algorithm:
+1.start
+2.read n and array elements
+3.read serach element key
+4.traverse array and compare each element with key
+5.if found,print position and stop
+6.Else print "Not found"
+7.stop
+  
+Program:
+  #include <stdio.h>
+int main()
+{
+    int size, elementToFind, i;
+    printf("Enter the size of Data Set for Linear Search:");
+    scanf("%d",&size);
+
+    int searchSpace[size];
+    for(i=0; i<=size-1; i++)
+    {
+        printf("Enter the %d out of %d element in data set: ",i+1, size);
+        scanf("%d",&searchSpace[i]);
+    }
+
+    //printf("Given Dataset for Linear Search is: ");
+    for(int i=0; i<size; i++)
+    {
+        printf("%d - ",searchSpace[i]);
+    }
+
+    printf("\nEnter the element to find in the dataset: ");
+    scanf("%d",&elementToFind);
+    i =0;
+    while(i<size)
+    {
+        if(searchSpace[i] == elementToFind)
+        {
+            printf("Required element %d is found in the dataset at position %d or index %d",elementToFind, i+1, i);
+            return 0;
+        }
+        i++;
+    }
+
+    printf("Required element %d is NOT found in the dataset!!!",elementToFind);
+}
