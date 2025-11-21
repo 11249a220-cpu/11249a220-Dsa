c81deb76d6ef110236db356c59d9359625d57fe1
11249a220-cpu <11249a220@kanchiuniv.ac.in>
2025-11-21 10:22:30 +0530

    Create TreeTraversalInDFS.c

---
+++ TreeTraversalInDFS.c
@@ -0,0 +1,111 @@
+Aim:
+
+To traverse a binary tree using Breadth First Search (BFS) technique and visit all nodes level by level.
+
+Algorithm (BFS / Level Order Traversal in C):
+
+Step 1: Start
+Step 2: If the tree is empty, print “Tree is empty” and stop
+Step 3: Create a queue and insert the root node into the queue
+Step 4: Repeat the following steps while the queue is not empty:
+    a) Remove (dequeue) a node from the front of the queue
+    b) Display the data of the node
+    c) If the left child exists, insert it into the queue
+    d) If the right child exists, insert it into the queue
+Step 5: Stop
+
+Program:
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure for tree node
+// Data, Pointer to left child and Pointer to right child
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};
+
+// Node creation
+struct Node *createNode(int value)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert the new node and returns root node of the updated tree
+struct Node *insert(struct Node *root, int value)
+{
+    // First node will be root node
+    if (root == NULL)
+    {
+        return createNode(value);
+    }
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+// Preorder - Root -> Left -> Right
+void preorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// Inorder - Left -> Root -> Right
+void inorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+// Postorder - Left -> Right -> Root
+void postorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main()
+{
+
+    int n, value;
+    struct Node *root = NULL;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d values:\n", n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &value);
+        root = insert(root, value); // Insert dynamically into BST
+    }
+
+    printf("Preorder Traversal: ");
+    preorder(root);
+
+    printf("\nInorder Traversal: ");
+    inorder(root);
+
+    printf("\nPostorder Traversal: ");
+    postorder(root);
+    return 0;
+}
